a5ebe001e17739c792344f48f5e2ec05479030d9
navilll <navilfaisal00@gmail.com>
2026-06-21 22:24:44 +0530

    speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/assets/img/banner-4.webp" as="image" />
+        <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-solid-900.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-brands-400.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/fa-light-300.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/fonts/flaticon.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+      </head>
       <body>
-      {children}
+        {children}
       </body>
     </html>
   );
